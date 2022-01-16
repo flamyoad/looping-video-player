edece7b57ac70105e9707841b92603f4039e15e6
flamyoad <yizake1820@gmail.com>
2022-01-16 14:23:58 +0800

    Didnt know Glide can load video uri directly

--- app/src/main/java/com/flamyoad/loopingvideoplayer/ui/video_list/VideoListBinding.kt
+++ app/src/main/java/com/flamyoad/loopingvideoplayer/ui/video_list/VideoListBinding.kt
@@ -22,20 +22,8 @@ fun bindRecyclerView(recyclerView: RecyclerView, list: List<Video>?) {
 @RequiresApi(Build.VERSION_CODES.Q)
 @BindingAdapter("imageUri")
 fun bindImage(imageView: ImageView, uri: Uri) {
-    val contentResolver = imageView.rootView.context.contentResolver
-    val thumbnail: Bitmap? = if ((Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q)) {
-        contentResolver.loadThumbnail(uri, Size(96, 96), null)
-    } else {
-        val id =
-            uri.toString().substringAfterLast("/").toLong()
-        MediaStore.Video.Thumbnails.getThumbnail(
-            contentResolver,
-            id,
-            MediaStore.Video.Thumbnails.MICRO_KIND,
-            null
-        )
-    }
     Glide.with(imageView)
-        .load(thumbnail)
+        .load(uri)
+        .dontTransform()
         .into(imageView)
 }
