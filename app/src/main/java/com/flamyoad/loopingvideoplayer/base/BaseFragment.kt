a482998a8038a5f729b2b15b3d4189bd64a998c4
flamyoad <yizake1820@gmail.com>
2022-02-13 00:58:10 +0800

    Fix

--- app/src/main/java/com/flamyoad/loopingvideoplayer/base/BaseFragment.kt
+++ app/src/main/java/com/flamyoad/loopingvideoplayer/base/BaseFragment.kt
@@ -2,13 +2,13 @@ package com.flamyoad.loopingvideoplayer.base
 
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import com.flamyoad.loopingvideoplayer.core.navigator.NavigationDispatcherImpl
+import com.flamyoad.loopingvideoplayer.core.navigator.NavigationDispatcher
 import javax.inject.Inject
 
 open class BaseFragment : Fragment() {
 
     @Inject
-    lateinit var navigator: NavigationDispatcherImpl
+    lateinit var navigator: NavigationDispatcher
 
     override fun onResume() {
         super.onResume()
